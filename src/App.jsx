3e03a6e95ea12b6ed2ce72979a5be6dcdc46e6d2
kareena0211 <kareena22@navgurukul.org>
2024-06-14 12:34:22 +0530

    Remove Normal CSS and Added Tailwind css into the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react';
-import Quiz from './components/Quiz';
-import questionsData from './questions.json';
-import "./index.css"
+import { useState } from "react";
+import Quiz from "./components/Quiz";
+import questionsData from "./questions.json";
+import "./index.css";
 
 function App() {
   const [start, setStart] = useState(false);
 
   return (
-    <div className="app-container">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       {!start ? (
-        <button onClick={() => setStart(true)} className="start-button">
+        <button
+          onClick={() => setStart(true)}
+          className="px-6 py-3 text-lg font-bold text-white bg-pink-500 rounded cursor-pointer"
+        >
           Start KBC Game
         </button>
       ) : (
