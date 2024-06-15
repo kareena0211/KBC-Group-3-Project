1bc06a8214f65387a0dc60214701c82ee8db1e0a
Amit Yadav <87850377+Amit-Yadav21@users.noreply.github.com>
2024-06-15 11:27:18 +0530

    Revert "Add login, signup and authentication backend code"

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginSignUp from "./components/LoginSignUp";
+
+const App = () => {
+  return (
+    <>
+      <LoginSignUp />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/LoginSignUp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoginSignUp = () => {
+  return <>Login SignUp Page it is new comit </>;
+};
+
+export default LoginSignUp;
