7dffcc49028e945312a8580608444cefc334abb6
kareena0211 <kareena22@navgurukul.org>
2024-06-18 11:24:59 +0530

    Added About page in Fronted folder , and Added routers too , and Added Media Query Too

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,23 +3,23 @@ import "./index.css";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import NavBar from "./Pages/NavBar";
-import Home from "./Pages/Home"
+import Home from "./Pages/Home";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./Pages/About";
 
 function App() {
-
   return (
     <>
       <Router>
-        <NavBar/>
+        <NavBar />
         <Routes>
           <Route path="/Home" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
         </Routes>
       </Router>
-
     </>
   );
 }
